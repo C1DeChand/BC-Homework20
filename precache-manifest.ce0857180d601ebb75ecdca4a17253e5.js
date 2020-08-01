da4445be8f4c273a357cb102e91791818ca04468
C1DeChand <C1DeChand@gmail.com>
2020-08-01 10:13:28 -0700

    Updates

---
+++ precache-manifest.ce0857180d601ebb75ecdca4a17253e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ce797ce9984240858892f3103544aa",
+    "url": "/BC-Homework20/index.html"
+  },
+  {
+    "revision": "87f5ce64ea7154f316b6",
+    "url": "/BC-Homework20/static/js/2.05e0255f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BC-Homework20/static/js/2.05e0255f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c142eacfb4fb25661814",
+    "url": "/BC-Homework20/static/js/main.b5ee4a09.chunk.js"
+  },
+  {
+    "revision": "2bc8381def4722c2eeb3",
+    "url": "/BC-Homework20/static/js/runtime-main.96589ecc.js"
+  },
+  {
+    "revision": "a7f6ddc18bc09b85aa9f350df54e535c",
+    "url": "/BC-Homework20/static/media/AboutPic.a7f6ddc1.jpg"
+  },
+  {
+    "revision": "8c542035b560af385bc28e735df6e789",
+    "url": "/BC-Homework20/static/media/Homework03.8c542035.png"
+  },
+  {
+    "revision": "4a7ff57c921135d8b999593785c3afc0",
+    "url": "/BC-Homework20/static/media/Homework05.4a7ff57c.png"
+  },
+  {
+    "revision": "26300c57b75a1c59e7a6eff16f167e30",
+    "url": "/BC-Homework20/static/media/Homework06.26300c57.png"
+  },
+  {
+    "revision": "b0d0aa76739f460c9de117e8968223ce",
+    "url": "/BC-Homework20/static/media/homework13.b0d0aa76.png"
+  },
+  {
+    "revision": "ab233634657d451b8e78f8649e9993c3",
+    "url": "/BC-Homework20/static/media/projectOne.ab233634.png"
+  },
+  {
+    "revision": "91ab59f013c247c7281ed8b557eaa316",
+    "url": "/BC-Homework20/static/media/projectTwo.91ab59f0.png"
+  }
+]);
